e0262c7c1bc89e2b9a0a0ef5761d83bc96fc39d7
Julio Condor <julioc3bgud@gmail.com>
2025-02-18 23:08:41 -0500

    Agregando pagina de prueba

--- front/src/app/page.js
+++ front/src/app/page.js
@@ -1,11 +1,13 @@
-import Rocket from '../assets/rocket.svg';
+import Rocket from "../assets/rocket.svg";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center pb-20 min-h-screen">
       <h2 className="text-xl font-bold text-center">Bienvenido a:</h2>
-      <h1 className="text-3xl font-bold text-center">Gestor de Tareas Colaborativo</h1>
-     
+      <h1 className="text-3xl font-bold text-center">
+        Gestor de Tareas Colaborativo
+      </h1>
+
       <Rocket className="w-24 h-24 text-blue-500" />
     </div>
   );

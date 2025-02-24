0d687ddcf15fe473364abcdecf639b9ede20abce
Diego Mateus <fcdiegomateus@gmail.com>
2025-02-24 17:54:02 -0500

    Fase intermedia Sidebar

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,22 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/i,
-        oneOf: [
-          {
-            issuer: /\.[jt]sx?$/,
-            use: ["@svgr/webpack"],
-          },
-          {
-            type: "asset/resource",
-          },
-        ],
-      });
-  
-      return config;
-    },
-  };
-  
-  export default nextConfig;
-  
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      oneOf: [
+        {
+          issuer: /\.[jt]sx?$/,
+          use: ["@svgr/webpack"],
+        },
+        {
+          type: "asset/resource",
+        },
+      ],
+    });
+
+    return config;
+  },
+};
+
+export default nextConfig;  
--- front/src/components/Sidebar.jsx
+++ front/src/components/Sidebar.jsx
@@ -1,23 +1,33 @@
+
 import Image from "next/image";
+import { useState } from "react";
 
 const Sidebar = () => {
+    const [isExpanded, setIsExpanded] = useState(false);
+    
     return(
-        <div>
-            <aside>
+            <aside 
+                className={`fixed top-0 left-0 h-full bg-secondary text-secondary-content transition-all duration-300 
+                    ${isExpanded ? "w-56" : "w-16"}`} 
+                onMouseEnter={() => setIsExpanded(true)}
+                onMouseLeave={() => setIsExpanded(false)}
+            >
                 <ul>
                     <li className="flex flex-row items-center gap-2">
                         <Image 
                             src="/logo.svg"
-                            alt="Ícono Home"
-                            width={9.72}
-                            height={auto}
+                            alt="Isologo Panal"
+                            width={38.88}
+                            height={37.88}
                         />
-                        <Image 
-                            src="/panal.svg"
-                            alt="Ícono Home"
-                            width={24}
-                            height={24}
-                        />
+                        <div className={`transition-opacity duration-300 ${isExpanded ? "opacity-100" : "opacity-0 overflow-hidden w-0"}`}>
+                            <Image 
+                                src="/panal.svg"
+                                alt="Tipografía Panal"
+                                width={68}
+                                height={26}
+                            />
+                        </div>
                     </li>
                     <li className="flex flex-row items-center gap-2">
                         <Image 
@@ -26,29 +36,37 @@ const Sidebar = () => {
                             width={24}
                             height={24}
                         />
-                        <a>Home</a>
+                        <a 
+                        className={`transition-opacity duration-300 ${isExpanded ? "opacity-100" : "opacity-0 overflow-hidden w-0"}`}>
+                            Home
+                        </a>
                     </li>
                     <li className="flex flex-row items-center gap-2">
                         <Image 
-                            src="/home-icon.svg"
-                            alt="Ícono Home"
+                            src="/tareas-icon.svg"
+                            alt="Ícono Tareas"
                             width={24}
                             height={24}
                         />
-                        <a>Home</a>
+                        <a 
+                        className={`transition-opacity duration-300 ${isExpanded ? "opacity-100" : "opacity-0 overflow-hidden w-0"}`}>
+                            Tareas
+                        </a>
                     </li>
                     <li className="flex flex-row items-center gap-2">
                         <Image 
-                            src="/home-icon.svg"
-                            alt="Ícono Home"
+                            src="/equipos-icon.svg"
+                            alt="Ícono Equipos"
                             width={24}
                             height={24}
                         />
-                        <a>Home</a>
+                        <a 
+                        className={`transition-opacity duration-300 ${isExpanded ? "opacity-100" : "opacity-0 overflow-hidden w-0"}`}>
+                            Equipos
+                        </a>
                     </li>
                 </ul>
             </aside>
-        </div>
     )
 } 
 

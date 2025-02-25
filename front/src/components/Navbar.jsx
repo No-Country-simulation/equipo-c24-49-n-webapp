aa37d3503343e297d3103ab3f610ee6295f6bc7e
rafopm <rafopm@gmail.com>
2025-02-24 20:37:25 -0500

    add landing && update layout

--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -7,54 +7,54 @@ const Navbar = () => {
   const pathname = usePathname();
 
   return (
-    <div className="navbar bg-base-100  px-4 md:px-8 max-w-screen-2xl mx-auto">
-      <div className="navbar-start">
-        <a href="/" className="btn btn-link flex items-center gap-2">
+    <div className="navbar bg-base-100 flex justify-between px-4 md:px-8 max-w-screen-2xl mx-auto">
+      {/* Logo al extremo izquierdo */}
+      <div className=" ">
+        <a href="/" className="btn btn-link flex items-center text-gray-900 gap-2 no-underline hover:no-underline active:no-underline">
           <img
             src="/logo.svg"
             alt="Logo"
             className="w-12 h-12 object-contain"
           />
-          <img
-            src="/panal.svg"
-            alt="Logo"
-            className="w-24 h-12 object-contain"
-          />
+          <span className="text-[24px] font-bold">PANAL</span>
         </a>
       </div>
 
-      <div className="navbar-center hidden md:flex">
-        <ul className="menu menu-horizontal gap-2">
+      {/* Menú y botones al extremo derecho */}
+      <div className=" gap-2 ">
+        {/* Menú */}
+        <ul className="menu menu-horizontal gap-2 text-sm font-semibold flex-nowrap mr-8 hidden md:flex">
           <li>
-            <a href="#features" className="px-2">
+            <a href="#features" >
               Recursos
             </a>
           </li>
           <li>
-            <a href="#pricing" className="px-2">
+            <a href="#pricing" >
               Equipos
             </a>
           </li>
           <li>
-            <a href="#contact" className="px-2">
+            <a href="#contact" >
               Sobre el proyecto
             </a>
           </li>
         </ul>
-      </div>
+        <div className="flex flex-nowrap gap-2">
+          {/* Botones de inicio de sesión y registro */}
+          {pathname !== "/login" && (
+            <Link href="/login" className="btn">
+              Iniciar Sesión
+            </Link>
+          )}
 
-      <div className="navbar-end gap-2">
-        {pathname !== "/login" && (
-          <Link href="/login" className="btn ">
-            Iniciar Sesión
-          </Link>
-        )}
+          {pathname !== "/register" && (
+            <Link href="/register" className="hidden md:flex btn btn-primary ">
+              Registrarse
+            </Link>
+          )}
+        </div>
 
-        {pathname !== "/register" && (
-          <Link href="/register" className="btn btn-primary ">
-            Registrarse
-          </Link>
-        )}
       </div>
     </div>
   );

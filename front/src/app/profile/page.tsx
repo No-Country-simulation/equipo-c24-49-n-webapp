6521ab7a011c0719c771ec7756fb460a2ab82752
Julio Condor <julioc3bgud@gmail.com>
2025-03-01 00:38:11 -0500

    add uploadthing

--- front/src/app/profile/page.tsx
+++ front/src/app/profile/page.tsx
@@ -1,17 +1,25 @@
 "use client";
 
+import { UploadButton } from "@/utils/uploadthing";
 import { useSession } from "next-auth/react";
+import { useState } from "react";
+import toast from 'react-hot-toast';
 
 const ProfilePage = () => {
   // Obtener la sesión del usuario
   const { data: session, status } = useSession();
+  const [imageUrl, setImageUrl] = useState<string | null>(null); // Estado para la URL de la imagen
 
   if (status === "loading") {
     return <p>Cargando...</p>;
   }
 
   if (!session) {
-    return <p>No has iniciado sesión. Por favor, inicia sesión para ver tu perfil.</p>;
+    return (
+      <p>
+        No has iniciado sesión. Por favor, inicia sesión para ver tu perfil.
+      </p>
+    );
   }
 
   return (
@@ -52,7 +60,42 @@ const ProfilePage = () => {
                   </span>
                 </label>
                 {/* @ts-ignore */}
-                <p className="text-neutral-700">{session.user?.role || "Usuario"}</p>
+                <p className="text-neutral-700">
+                  {session.user?.role || "Usuario"}
+                </p>
+              </div>
+
+              {/* Mostrar la imagen recién subida */}
+              {imageUrl && (
+                <div>
+                  <label className="label">
+                    <span className="label-text text-primary font-semibold">
+                      Imagen Subida
+                    </span>
+                  </label>
+                  <img
+                    src={imageUrl}
+                    alt="Imagen subida"
+                    className="w-full h-auto rounded-lg"
+                  />
+                </div>
+              )}
+
+              {/* Botón de subida de imágenes */}
+              <div className="mt-6">
+                <UploadButton
+                  endpoint="imageUploader"
+                  onClientUploadComplete={(res) => {
+                    // Obtener la URL de la imagen recién subida
+                    if (res && res.length > 0) {
+                      setImageUrl(res[0].url); // Actualizar el estado con la URL de la imagen
+                      toast.success("Subida completada");
+                    }
+                  }}
+                  onUploadError={(error: Error) => {
+                    toast.error(`ERROR! ${error.message}`);
+                  }}
+                />
               </div>
             </div>
           </div>

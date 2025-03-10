d48ec2f6a3f793e5c7ba77c83b5f8a6294982063
Julio Condor <julioc3bgud@gmail.com>
2025-03-10 12:28:26 -0500

    Add style

--- front/src/app/dashboard/layout.js
+++ front/src/app/dashboard/layout.js
@@ -2,12 +2,29 @@
 
 import NavBarDashboard from "@/components/NavBarDashboard"
 import Sidebar from "@/components/Sidebar"
+import { Geist, Geist_Mono, Kodchasan } from "next/font/google";
 
+
+const kodchasan = Kodchasan({
+  variable: "--font-kodchasan",
+  weight: "400",
+  subsets: ["latin"],
+});
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export default function Layout({ children }) {
   return (
 
-    <main className="relative flex flex-row w-full min-h-screen">
+    <main className={`${geistSans.variable} ${geistMono.variable} ${kodchasan.variable}  relative flex flex-row w-full min-h-screen`}>
       <Sidebar />
       <div className="flex-1">
         <header className="w-full">
--- front/src/app/dashboard/page.js
+++ front/src/app/dashboard/page.js
@@ -8,15 +8,15 @@ export default function Page() {
     <div className="flex flex-col item-center text-secondary-content">
             
       <section className={`${kodchasan.className}`}>
-        <h1 className={`${kodchasan.className} ml-28 text-[24px]`}>
+        <h1 className={`${kodchasan.className} ml-[78px] text-[24px] mb-3`}>
           Escritorio
         </h1>
         <DashboardCard/>
       </section>
 
-      <section className="flex flex-row justify-between ml-16 mt-10 mr-14 mb-10">
-        <img src="./ejemplo-progreso.svg" alt="Imagen ejemplo progreso"/>
-        <img src="./ejemplo-calendario.svg" alt="Imagen ejemplo calendario"/>
+      <section className="flex flex-row justify-between ml-[45px] mt-[55px] mr-14  ">
+        <img src="./ejemplo-progreso.svg" alt="Imagen ejemplo progreso" className="w-fit"/>
+        <img src="./ejemplo-calendario.svg" alt="Imagen ejemplo calendario" className="w-fit"/>
       </section>
     </div>
   )
